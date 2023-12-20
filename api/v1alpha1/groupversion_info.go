ef2e3054df415377ef13d6d0bfd2ff70c97fc308
Artem Bortnikov <artem.bortnikov@telekom.com>
2023-12-20 22:42:54 +0200

    initial
    Signed-off-by: Artem Bortnikov <artem.bortnikov@telekom.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,29 +1,36 @@
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
-// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright 2023.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package v1alpha1 contains API Schema definitions for the lifecycle v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=lifecycle.ironcore.dev
 package v1alpha1
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	runtimeutil "k8s.io/apimachinery/pkg/util/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
-	// GroupVersion is a group & version definition for provided API types.
-	GroupVersion = schema.GroupVersion{Group: "metal.ironcore.dev", Version: "v1alpha1"}
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "lifecycle.ironcore.dev", Version: "v1alpha1"}
 
-	// SchemeBuilder builds a new scheme to map provided API types to kubernetes GroupVersionKind.
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-)
 
-// AddToScheme adds provided API types to scheme.Builder and adds them to runtime.Scheme.
-func AddToScheme(scheme *runtime.Scheme) {
-	SchemeBuilder.Register(&MachineLifecycle{}, &MachineLifecycleList{})
-	SchemeBuilder.Register(&MachineType{}, &MachineTypeList{})
-	SchemeBuilder.Register(&FirmwarePackage{}, &FirmwarePackageList{})
-	SchemeBuilder.Register(&UpdateTask{}, &UpdateTaskList{})
-	SchemeBuilder.Register(&MachineUpdateJob{}, &MachineUpdateJobList{})
-	runtimeutil.Must(SchemeBuilder.AddToScheme(scheme))
-}
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
